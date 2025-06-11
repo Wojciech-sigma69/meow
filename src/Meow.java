73aef2fee719ee4b66f6c81c9eab53bdd5414082
Wojciech Sopel <wojciech.sopel24@my.northampton.ac.uk>
2025-06-11 14:10:02 +0100

    my first code

---
+++ src/Meow.java
@@ -0,0 +1,8 @@
+public class Meow {
+    public static void main(String[] args) throws Exception{
+        String kitty = "meow";
+        float meows = 50.74854f;
+        String animal = "kitties :D";
+        System.out.println(kitty + " has " + meows + " " + animal);
+    }
+}
